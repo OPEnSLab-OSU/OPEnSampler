00f366008259f960e017ffa58a0bc8fed689c11b
hunterl31 <hunt.lien@gmail.com>
2017-10-17 11:10:15 -0700

    Added blank android studio project with compatibility for Android 5.0 and up

---
+++ OPEnSamplerApp/app/src/main/java/org/open_sensing/opensamplerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package org.open_sensing.opensamplerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
